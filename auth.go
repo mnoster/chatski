a68533567beea81da45e6786905356c5d892682e
Nicholas Porter <nicholas@metax.io>
2018-11-26 23:13:17 -0800

    Google auth req working

--- auth.go
+++ auth.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"github.com/stretchr/gomniauth"
 	"log"
 	"net/http"
 	"strings"
@@ -32,12 +33,23 @@ func MustAuth(handler http.Handler) http.Handler {
 // loginHandler handles 3rd party login process
 // format: /auth/{action}/{provider}
 func loginHandler(w http.ResponseWriter, r *http.Request) {
+
 	segs := strings.Split(r.URL.Path, "/")
 	action := segs[2]
 	provider := segs[3]
+
 	switch action {
 	case "login":
-		log.Println("TODO handle login for", provider)
+		provider, err := gomniauth.Provider(provider)
+		if err != nil {
+			log.Fatalln("Error when trying to get provider", provider)
+		}
+		loginUrl, err := provider.GetBeginAuthURL(nil, nil)
+		if err != nil {
+			log.Fatalln("Error when trying to GetBeginAuthURL for", provider, "-", err)
+		}
+		w.Header().Set("Location", loginUrl)
+		w.WriteHeader(http.StatusTemporaryRedirect)
 	default:
 		w.WriteHeader(http.StatusNotFound)
 		fmt.Fprintf(w, "Auth action %s not supported", action)
--- main.go
+++ main.go
@@ -3,6 +3,10 @@ package main
 import (
 	"flag"
 	"github.com/mnoster/chat/trace"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 	"html/template"
 	"log"
 	"net/http"
@@ -12,7 +16,6 @@ import (
 )
 
 // templ represents a single template
-
 type templateHandler struct {
 	once     sync.Once
 	filename string
@@ -31,6 +34,16 @@ func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 
 	flag.Parse()
+
+	gomniauth.SetSecurityKey("x1x2x3x4x5")
+
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+
+		google.New("799101463365-k562r8pslupda1kvvfdidjkkqvd13gm8.apps.googleusercontent.com", "VXzzOx1BQXuITz_2JUzH0vzd", "http://localhost:8080/auth/callback/google"),
+
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+	)
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
